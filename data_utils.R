d28485d78096d358b0fe011477af9a62499459b5
girelaignacio <girelaignacio@gmail.com>
2023-04-13 00:10:08 -0300

    new data functions and more beatiful plts

--- data_utils.R
+++ data_utils.R
@@ -47,38 +47,11 @@ data_list <- MPI_colnames(data_list)
 
 data <- Reduce(function(x, y) merge(x, y, all=T), data_list)
 
-
-national_total <- openxlsx::read.xlsx("https://ophi.org.uk/wp-content/uploads/Table-1-National-Results-MPI-2022.xlsx",
-                                sheet = "1.1 National MPI Results", startRow = 9, colNames = FALSE)[1:111,2:9]
-colnames(national_total) <- c("ISO","country", "world_region", "survey", "year","MPI","H","A")
-
-national_censH <- openxlsx::read.xlsx("https://ophi.org.uk/wp-content/uploads/Table-1-National-Results-MPI-2022.xlsx",
-                                      sheet = "1.2 Censored Headcounts", startRow = 9, colNames = FALSE)[,c(2:6,8:17)]
-colnames(national_censH) <- c("ISO","country", "world_region", "survey", "year",
-                              "nutrition","child mortality",
-                              "years of schooling", "school attendance", 
-                              "cooking fuel", "sanitation", "drinking water","electricity","housing","assets")
-
-national_contr <- openxlsx::read.xlsx("https://ophi.org.uk/wp-content/uploads/Table-1-National-Results-MPI-2022.xlsx",
-                                      sheet = "1.3 Contribut'n of Deprivations", startRow = 9, colNames = FALSE)[,c(2:6,11:20)]
-colnames(national_contr) <- c("ISO","country", "world_region", "survey", "year",
-                              "ctr_nutrition","ctr_child mortality",
-                              "ctr_years of schooling", "ctr_school attendance", 
-                              "ctr_cooking fuel", "ctr_sanitation", "ctr_drinking water","ctr_electricity","ctr_housing","ctr_assets")
-national_other <- openxlsx::read.xlsx("https://ophi.org.uk/wp-content/uploads/Table-1-National-Results-MPI-2022.xlsx",
-                                      sheet = "1.4 MPI Results & Compl. Data", startRow = 9, colNames = FALSE)[1:111,c(2:6,9,11,13,15:17,19)]
-colnames(national_other) <- c("ISO","country", "world_region", "survey", "year",
-                              "1.9USD","3.10USD","NationalPovertyLine","GNIpercapita",
-                              "Gini","HDI","IncomeCategory")
-
-
-
-
-
-df_list <- list(national_total, national_censH, national_contr, national_other)
-data <- Reduce(function(x, y) merge(x, y, all=T), df_list)
+# Order data
 data$ISO <- factor(data$ISO, levels = data$ISO[order(data$MPI)])
-data$world_region <- as.factor(data$world_region)
+data$Ranking <- seq(1:length(data$ISO))
+data$Country <- as.factor(data$Country)
+data$`World Region` <- as.factor(data$`World Region`)
 data$ARG <- ifelse(data$ISO == "ARG", "yes", "no")
-data <- data[complete.cases(data$ISO),]
+
 
--- plots.R
+++ plots.R
@@ -1,56 +1,71 @@
 library(ggplot2)
 
+arg_MPI <- data$MPI[data$Country == "Argentina"]
+
 ##### MPI global all of the world
-ggplot(data, aes(x=ISO, y=MPI, fill=world_region)) + 
+ggplot(data, aes(x=ISO, y=MPI, fill=`World Region`)) + 
   geom_bar(position='dodge', stat = "identity") + theme_minimal() +
   theme(#legend.key.size = unit(1, 'cm'), #change legend key size
         #legend.key.height = unit(1, 'cm'), #change legend key height
         #legend.key.width = unit(1, 'cm'), #change legend key width
         legend.title = element_text(size=8), #change legend title font size
         legend.text = element_text(size=5),
-        axis.text.x=element_text(angle=90,hjust=1)) + xlab("")
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust=1, size = 5)) + xlab("") +
+  annotate("segment", x = 8, y = arg_MPI + 0.03, xend = 8, yend = arg_MPI,
+           arrow = arrow(type = "closed", length = unit(0.02, "npc")))
 
-### MPI Latin America and Argentina ####
-ggplot(data[data$world_region == "Latin America and the Caribbean",], aes(x=ISO, y=MPI, fill=ARG)) + 
+ggplot(data = data, mapping = aes(x = `World Region`, y = MPI, fill=`World Region`)) +
+  geom_boxplot() +
+  labs(x=NULL, y="MPI") +
+  theme(legend.position = "none")
+
+  ### MPI Latin America and Argentina ####
+ggplot(data[data$`World Region` == "Latin America and the Caribbean",], aes(x=ISO, y=MPI, fill=ARG)) + 
   geom_bar(stat = "identity") + theme_minimal() +
   theme(axis.text.x=element_text(angle=90,hjust=1),
         legend.position = "none") + xlab("") +
-  scale_fill_manual( values = c( "yes"="tomato", "no"="gray" ) )
+  annotate("segment", x = 1, y = arg_MPI + 0.03, xend = 1, yend = arg_MPI,
+           arrow = arrow(type = "closed", length = unit(0.02, "npc")))
 
 ### MPI Arab States and Argentina ####
-ggplot(data[(data$world_region == "Arab States") | (data$country == "Argentina"),], aes(x=ISO, y=MPI,fill=ARG)) + 
+ggplot(data[(data$`World Region` == "Arab States") | (data$Country == "Argentina"),], aes(x=ISO, y=MPI,fill=ARG)) + 
   geom_bar(stat = "identity") + theme_minimal() +
   theme(axis.text.x=element_text(angle=90,hjust=1),
         legend.position = "none") + xlab("") +
-  scale_fill_manual( values = c( "yes"="tomato", "no"="gray" ) )
+  annotate("segment", x = 1, y = arg_MPI + 0.03, xend = 1, yend = arg_MPI,
+           arrow = arrow(type = "closed", length = unit(0.02, "npc")))
 
 ### MPI East Asia and the Pacific and Argentina ####
-ggplot(data[(data$world_region == "East Asia and the Pacific") | (data$country == "Argentina"),], aes(x=ISO, y=MPI, fill=ARG)) + 
+ggplot(data[(data$`World Region` == "East Asia and the Pacific") | (data$Country == "Argentina"),], aes(x=ISO, y=MPI, fill=ARG)) + 
   geom_bar(stat = "identity") + theme_minimal() +
   theme(axis.text.x=element_text(angle=90,hjust=1),
         legend.position = "none") + xlab("") +
-  scale_fill_manual( values = c( "yes"="tomato", "no"="gray" ) )
+  annotate("segment", x = 1, y = arg_MPI + 0.03, xend = 1, yend = arg_MPI,
+           arrow = arrow(type = "closed", length = unit(0.02, "npc")))
 
 ### MPI Europe and Central Asia and Argentina ####
-ggplot(data[(data$world_region == "Europe and Central Asia") | (data$country == "Argentina"),], aes(x=ISO, y=MPI, fill=ARG)) + 
+ggplot(data[(data$`World Region` == "Europe and Central Asia") | (data$Country == "Argentina"),], aes(x=ISO, y=MPI, fill=ARG)) + 
   geom_bar(stat = "identity") + theme_minimal() +
   theme(axis.text.x=element_text(angle=90,hjust=1),
         legend.position = "none") + xlab("") +
-  scale_fill_manual( values = c( "yes"="tomato", "no"="gray" ) )
+  annotate("segment", x = 8, y = arg_MPI + 0.005, xend = 8, yend = arg_MPI,
+           arrow = arrow(type = "closed", length = unit(0.02, "npc")))
 
 ### MPI South Asia and Argentina ####
-ggplot(data[(data$world_region == "South Asia") | (data$country == "Argentina"),], aes(x=ISO, y=MPI, fill=ARG)) + 
+ggplot(data[(data$`World Region` == "South Asia") | (data$Country == "Argentina"),], aes(x=ISO, y=MPI, fill=ARG)) + 
   geom_bar(stat = "identity") + theme_minimal() +
   theme(axis.text.x=element_text(angle=90,hjust=1),
         legend.position = "none") + xlab("") +
-  scale_fill_manual( values = c( "yes"="tomato", "no"="gray" ) )
+  annotate("segment", x = 1, y = arg_MPI + 0.03, xend = 1, yend = arg_MPI,
+           arrow = arrow(type = "closed", length = unit(0.02, "npc")))
 
 ### MPI Sub-Saharan Africa and Argentina ####
-ggplot(data[(data$world_region == "Sub-Saharan Africa") | (data$country == "Argentina"),], aes(x=ISO, y=MPI, fill=ARG)) + 
+ggplot(data[(data$`World Region` == "Sub-Saharan Africa") | (data$Country == "Argentina"),], aes(x=ISO, y=MPI, fill=ARG)) + 
   geom_bar(stat = "identity") + theme_minimal() +
   theme(axis.text.x=element_text(angle=90,hjust=1),
         legend.position = "none") + xlab("") +
-  scale_fill_manual( values = c( "yes"="tomato", "no"="gray" ) )
+  annotate("segment", x = 1, y = arg_MPI + 0.03, xend = 1, yend = arg_MPI,
+           arrow = arrow(type = "closed", length = unit(0.02, "npc")))
 
 
 
@@ -75,7 +90,8 @@ ggplot(data=indicators, aes(x=country, y=value, fill=variable)) +
   geom_bar(stat="identity") +labs(fill="Indicators") + theme_classic()
 
 
-ggplot(data = dat, mapping = aes(x = CNT, y = science)) +
-  geom_boxplot() +
-  labs(x=NULL, y="Science Scores") +
-  theme_bw()
+ggplot(data = data, aes(x = MPI, y = `1.90 USD`)) + 
+  geom_point(stat = "identity")
+
+ggplot(data = data, aes(x = MPI, y = `3.10 USD`)) + 
+  geom_point(stat = "identity")
